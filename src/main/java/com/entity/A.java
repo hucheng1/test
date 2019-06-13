ab857a3e0c768275db0bb772f762f63350f5ffbf
Mr Hu <https://github.com/yugeweiwu/huanban0.1.git>
2019-06-13 21:01:49 +0800

    test

--- src/main/java/com/entity/A.java
+++ src/main/java/com/entity/A.java
@@ -1,6 +1,9 @@
 package com.entity;
 
 public class A {
-    private String aa ;
+    private String kkk ;
     private int agej;
+    public static void main(String args [] ){
+        System.out.println("sss");
+    }
 }
