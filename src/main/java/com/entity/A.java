faffb55230647323da46c14c11842e19c5c155a2
Mr Hu <https://github.com/yugeweiwu/huanban0.1.git>
2019-06-18 21:40:14 +0800

    Squashed commit of the following:
    commit 69bcce39e404b92a6066515725f767353d3daff0
    Author: Mr Hu <https://github.com/yugeweiwu/huanban0.1.git>
    Date:   Thu Jun 13 21:06:28 2019 +0800
    
        test

--- src/main/java/com/entity/A.java
+++ src/main/java/com/entity/A.java
@@ -4,7 +4,7 @@ public class A {
     private String kkk ;
     private int agej;
     public static void main(String args [] ){
-        System.out.println("sssss");
+        System.out.println(1112222213);
         System.out.println("sss");
     }
 }
